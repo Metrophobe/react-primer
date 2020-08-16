c09de9ac6500198a6147ef6b8cbe7a0052321214
= <=>
2020-08-16 20:56:05 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Person from "./Person/Person";
+
 import "./App.css";
 
 function App() {
@@ -6,6 +8,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>React Introduction</h1>
+        <Person name="Peter Pan" age="29" hobby="racing"></Person>
+        <Person name="Peter Borg" age="29">
+          Hobby:Racing
+        </Person>
+        <Person name="Sam Right" age="29" />
+        <Person name="Brian May" age="29" />
       </header>
     </div>
   );
