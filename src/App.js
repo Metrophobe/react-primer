6cf7ce59e5688e3a706031e5f675e237e76280d6
= <=>
2020-08-16 22:18:40 +0200

    ex 3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Person from "./Person/Person";
 
 import "./App.css";
 
 function App() {
+  let [persons, setPersons] = useState([
+    { name: "Tom", age: 27 },
+    { name: "Dick", age: 28 },
+    { name: "Harry", age: 26 },
+  ]);
+
+  let switcHandler = () => {
+    setPersons([
+      { name: "Jones", age: 27 },
+      { name: "Dastardly", age: 28 },
+      { name: "Potter", age: 26 },
+    ]);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Introduction</h1>
-        <Person name="Peter Pan" age="29" hobby="racing"></Person>
-        <Person name="Peter Borg" age="29">
-          Hobby:Racing
-        </Person>
-        <Person name="Sam Right" age="29" />
-        <Person name="Brian May" age="29" />
+        <button onClick={switcHandler}> Switch Name</button>
+        <Person name={persons[0].name} age={persons[0].age}></Person>
+        <Person name={persons[1].name} age={persons[2].age}></Person>
+        <Person name={persons[2].name} age={persons[2].age}></Person>
       </header>
     </div>
   );
