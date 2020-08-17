d74461b1b7bfa353cece77fbc8dcb512f476b8b6
= <=>
2020-08-18 00:21:03 +0200

    'ex4-passing-refs'

--- src/App.js
+++ src/App.js
@@ -4,26 +4,42 @@ import Person from "./Person/Person";
 import "./App.css";
 
 function App() {
+  let [state, setState] = useState(true);
+
   let [persons, setPersons] = useState([
     { name: "Tom", age: 27 },
     { name: "Dick", age: 28 },
     { name: "Harry", age: 26 },
   ]);
 
-  let switcHandler = () => {
-    setPersons([
-      { name: "Jones", age: 27 },
-      { name: "Dastardly", age: 28 },
-      { name: "Potter", age: 26 },
-    ]);
+  let switchHandler = () => {
+    if (state === true) {
+      setPersons([
+        { name: "Jones", age: 27 },
+        { name: "Dastardly", age: 28 },
+        { name: "Potter", age: 26 },
+      ]);
+      setState(false);
+    } else {
+      setPersons([
+        { name: "Tom", age: 27 },
+        { name: "Dick", age: 28 },
+        { name: "Harry", age: 26 },
+      ]);
+      setState(true);
+    }
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Introduction</h1>
-        <button onClick={switcHandler}> Switch Name</button>
-        <Person name={persons[0].name} age={persons[0].age}></Person>
+        <button onClick={switchHandler}> Switch Name</button>
+        <Person
+          click={switchHandler}
+          name={persons[0].name}
+          age={persons[0].age}
+        ></Person>
         <Person name={persons[1].name} age={persons[2].age}></Person>
         <Person name={persons[2].name} age={persons[2].age}></Person>
       </header>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+import "./Person.css";
+
 const person = (props) => {
   return (
     <div>
-      <p>
-        I am a Person named {props.name} I am {props.age}
+      <p onClick={props.click}>
+        I am a Person named {props.name} and I am {props.age}
       </p>
       <p> {props.children} </p>
     </div>
