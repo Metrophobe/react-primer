9fa15de023e3d2861bc954f16d48bd77fc8397b8
= <=>
2020-08-25 03:42:26 +0200

    ex7-conditional-rendering-sep-logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import Person from "./Person/Person";
-
 import "./App.css";
 
 function App() {
-  let [state, setState] = useState(true);
   let [visible, setVisible] = useState(false);
+
+  let [person, setPerson] = useState({
+    name: "name",
+    age: 0,
+  });
 
   let [persons, setPersons] = useState([
     { name: "Tom", age: 27 },
@@ -17,40 +20,88 @@ function App() {
     setVisible(!visible);
   };
 
-  let switchHandler = () => {
-    if (state) {
-      setPersons([
-        { name: "Jones", age: 27 },
-        { name: "Dastardly", age: 28 },
-        { name: "Potter", age: 26 },
-      ]);
-      setState(false);
-    } else {
-      setPersons([
-        { name: "Tom", age: 27 },
-        { name: "Dick", age: 28 },
-        { name: "Harry", age: 26 },
-      ]);
-      setState(true);
-    }
+  let addPersonHandler = () => {
+    const newPersons = [...persons];
+    newPersons.push(person);
+    setPersons(newPersons);
   };
+
+  let editPersonHandler = (e, personIndex) => {
+    const newPersons = [...persons];
+    persons[personIndex].name = e.target.value;
+    setPersons(newPersons);
+  };
+
+  //This handler is firing instantly and is not waiting for click
+  let deletePersonHandler = (personIndex) => {
+    const newPersons = [...persons];
+    newPersons.splice(personIndex, 1);
+    setPersons(newPersons);
+  };
+
+  //Adding new Person
+  let setNewNameHandler = (e) => {
+    setPerson({
+      name: e.target.value,
+      age: person.age,
+    });
+  };
+
+  let setNewAgeHandler = (e) => {
+    setPerson({
+      name: person.name,
+      age: e.target.value,
+    });
+  };
+
+  //Pre-process before render
+
+  let personsOutput = null;
+
+  if (visible) {
+    personsOutput = (
+      <div>
+        {persons.map((p, index) => (
+          <Person
+            key={index}
+            id={index}
+            click={showHideHandler}
+            delete={deletePersonHandler}
+            change={editPersonHandler}
+            name={p.name}
+            age={p.age}
+          ></Person>
+        ))}
+      </div>
+    );
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Introduction</h1>
+        <h3> Persons Held in Collection {persons.length}</h3>
+        <div>
+          <label> Name </label>
+          <input
+            type="text"
+            placeholder="name"
+            onChange={setNewNameHandler}
+            value={person.name}
+          />
+          <label> Age </label>
+          <input
+            type="text"
+            placeholder="age"
+            onChange={setNewAgeHandler}
+            value={person.age}
+          />
+          &nbsp;
+          <button onClick={addPersonHandler}>Add Person</button>
+        </div>
+        <br />
         <button onClick={showHideHandler}> Toggle Persons</button>
-        {visible
-          ? persons.map((p) => (
-              <div>
-                <Person
-                  click={switchHandler}
-                  name={p.name}
-                  age={p.age}
-                ></Person>
-              </div>
-            ))
-          : null}
+        {personsOutput}
       </header>
     </div>
   );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,15 +2,21 @@ import React from "react";
 
 import "./Person.css";
 
+//it seems like the onclick handler is not working here and is fired immediately (the one in the button)
 const person = (props) => {
   return (
-    <div className="Person">
+    <div className="Person" id={props.id}>
       <p onClick={props.click}>
         I am a Person named {props.name} and I am {props.age}
       </p>
       Type in Name{" "}
-      <input type="text" onChange={props.change} value={props.name} />
+      <input
+        type="text"
+        onChange={(e) => props.change(e, props.id)}
+        value={props.name}
+      />
       <p> {props.children} </p>
+      <button onClick={() => props.delete(props.id)}>Delete </button>
     </div>
   );
 };
